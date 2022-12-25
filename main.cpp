56780a05ecc5cb30b7333ebf778f07e14ca9a0a4
Jonathan <jonathanzakharov@gmail.com>
2022-12-24 19:44:30 -0800

    Revert "sfml boilerplate"
    This reverts commit 72de153a93935d96c6543d962f7187691bdd9c95.

--- main.cpp
+++ main.cpp
@@ -1,10 +1,24 @@
-#include "Program.hpp"
+#include <SFML/Graphics.hpp>
 
 int main()
 {
-    Program p;
+    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
 
-    p.start();
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
 
     return 0;
 }
